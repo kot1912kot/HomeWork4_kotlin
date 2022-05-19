8190ec18c88a7e68bf045ded158ffb620783a605
Mikhail <kot1912kot@gmail.com>
2022-05-19 20:59:15 +0300

    Добавили тестов

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -5,13 +5,67 @@ import org.junit.jupiter.api.Assertions.*
 internal class MainKtTest {
 
     @Test
-    fun commission() {
+    fun commissionVisa() {
         val typeCard = "Visa"
         val amountThisMounth = 3000
         val transferAmount = 15_000
 
         val result = commission(typeCard, amountThisMounth, transferAmount)
 
-        assertEquals(0, result)
+        Assert.assertEquals(112500, result)
+    }
+    @Test
+    fun commissionVkPay() {
+        val typeCard = "Vk Pay"
+        val amountThisMounth = 3000
+        val transferAmount = 15_000
+
+        val result = commission(typeCard, amountThisMounth, transferAmount)
+
+        Assert.assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionMaestro() {
+        val typeCard = "Maestro"
+        val amountThisMounth = 3000
+        val transferAmount = 15_000
+
+        val result = commission(typeCard, amountThisMounth, transferAmount)
+
+        Assert.assertEquals(0, result)
+    }
+
+    @Test
+    fun commission() {
+        val typeCard = "Union"
+        val amountThisMounth = 3000
+        val transferAmount = 15_000
+
+        val result = commission(typeCard, amountThisMounth, transferAmount)
+
+        Assert.assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionMir() {
+        val typeCard = "Мир"
+        val amountThisMounth = 3000
+        val transferAmount = 15_000
+
+        val result = commission(typeCard, amountThisMounth, transferAmount)
+
+        Assert.assertEquals(0, result)
+    }
+
+    @Test
+    fun commissionMastercard() {
+        val typeCard = "Mastercard"
+        val amountThisMounth = 3000
+        val transferAmount = 15_000
+
+        val result = commission(typeCard, amountThisMounth, transferAmount)
+
+        Assert.assertEquals(0, result)
     }
 }
